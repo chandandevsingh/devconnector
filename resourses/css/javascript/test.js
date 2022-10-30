b4e390e127028c01d1560a90f2a899aad905cd40
chandandevsingh <chandan.singh@stud.fra-uas>
2022-10-30 19:38:37 +0100

    commit4addjsandmodifiehtmlpages

---
+++ resourses/css/javascript/test.js
@@ -0,0 +1,5 @@
+var a =10;
+var b =11;
+var c = a+b;
+console.log(" hello");
+console.log("result is" +c);
---
+++ resourses/css/javascript/test4.js
@@ -0,0 +1,5 @@
+var result = 1;
+for (var i = 5; i >= 1; i--) 
+{
+    result = result * i;
+}
